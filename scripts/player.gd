aead7e3bcb720dd58b878bb53ccfe47b9dddb548
AniKrisn <anirudh.krishnan@hotmail.com>
2025-08-16 01:36:40 +0100

    add movement animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,6 +11,7 @@ const DEC = 5 * MULT
 const MAX_SPEED = 40 * MULT
 const unitVec = Vector2(1,1)
 
+@onready var usualPlayer = $CollisionShape2D/UsualPlayer
 @onready var animPlayer = $CollisionShape2D/AnimationPlayer
 # @onready var idleSprite = $CollisionShape2D/Sprite2D2
 # @onready var movingSprite = $CollisionShape2D/Sprite2D
@@ -18,6 +19,13 @@ const unitVec = Vector2(1,1)
 @onready var playerArea = $CollisionShape2D
 
 var isAttacking = false
+var counter = 0
+
+func play_animation(anim_name: StringName) -> void:
+	if !isAttacking:
+		counter+=1
+		#print(str(counter)+str(anim_name))
+		animPlayer.play(anim_name)
 
 func update_level():
 	# level = 3
@@ -46,15 +54,41 @@ func _physics_process(delta: float) -> void:
 	# animPlayer.play("moving")
 	
 	if Input.is_action_just_pressed("attack"):
-		isAttacking = true
 		animPlayer.play("attack")
 	
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	
+
+	if Input.is_key_pressed(KEY_LEFT):
+		usualPlayer.rotation_degrees = 0
+		usualPlayer.flip_h = false
+		play_animation("accelerate_H")
+	if Input.is_key_pressed(KEY_RIGHT):
+		usualPlayer.rotation_degrees = 0
+		usualPlayer.flip_h = true
+		play_animation("accelerate_H")
+		
+	if Input.is_key_pressed(KEY_LEFT) and Input.is_key_pressed(KEY_UP):
+		usualPlayer.flip_h = false
+		play_animation("accelerate_D")
+	elif Input.is_key_pressed(KEY_RIGHT) and Input.is_key_pressed(KEY_UP):
+		usualPlayer.flip_h = true
+		play_animation("accelerate_D")
+	elif Input.is_key_pressed(KEY_LEFT) and Input.is_key_pressed(KEY_DOWN):
+		usualPlayer.flip_h = false
+		usualPlayer.rotation_degrees = -90
+		play_animation("accelerate_D")
+	elif Input.is_key_pressed(KEY_RIGHT) and Input.is_key_pressed(KEY_DOWN):
+		usualPlayer.flip_h = true
+		usualPlayer.rotation_degrees = 90
+		play_animation("accelerate_D")
+
 	# altJitterFixer.before(position)
-	
 	if direction.x:
 		velocity.x = move_toward(velocity.x, sign(direction.x) * MAX_SPEED, abs(direction.x * ACC))
+
+
+		#animPlayer.play("accelerate_H")
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, DEC)
 
@@ -62,6 +96,8 @@ func _physics_process(delta: float) -> void:
 		velocity.y = move_toward(velocity.y, sign(direction.y) * MAX_SPEED, abs(direction.y * ACC))
 	else:
 		velocity.y = move_toward(velocity.y, 0, DEC)
+
+
 		
 	move_and_slide()
 	position = jitterFixer.fix_jitter(direction, position)
@@ -81,7 +117,9 @@ func _on_attack_area_entered(area: Area2D) -> void:
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	animPlayer.play("RESET")
+	if (anim_name == "attack"):
+		isAttacking = false
+		play_animation("accelerate_H")
 
 
 func _on_attack_body_entered(body: Node2D) -> void:
@@ -92,3 +130,12 @@ func _on_attack_body_entered(body: Node2D) -> void:
 func _on_attack_area_exited(area: Area2D) -> void:
 	if area.is_in_group("Breakable"):
 		area.stop_taking_damage()
+
+
+func _on_animation_player_animation_started(anim_name: StringName) -> void:
+	if (anim_name == "attack"):
+		isAttacking = true
+	if (anim_name == "accelerate_H"):
+		print("yea-35")
+	
+	
